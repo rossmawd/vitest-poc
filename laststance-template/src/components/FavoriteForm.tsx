9f49abba7d96c83d149b7ca0d3bdd74e414c35ca
Ross Mawdsley <48100198+rossmawd@users.noreply.github.com>
2023-07-27 19:38:30 +0100

    improved form appearance

--- laststance-template/src/components/FavoriteForm.tsx
+++ laststance-template/src/components/FavoriteForm.tsx
@@ -17,54 +17,57 @@ const MyForm: React.FC = () => {
   } = useFavoriteForm()
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="gap-y-6 flex flex-col items-center">
-        <label>
-          Name:{' '}
-          <input
-            type="text"
-            name="name"
-            value={formValues.name}
-            onChange={handleInputChange}
-            aria-invalid={errors.nameError ? 'true' : 'false'}
-            aria-describedby="nameError"
-          />
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-6">
+          <label className="flex justify-end">
+            <span className="pr-2">Name: </span>
+            <input
+              type="text"
+              name="name"
+              value={formValues.name}
+              onChange={handleInputChange}
+              aria-invalid={errors.nameError ? 'true' : 'false'}
+              aria-describedby="nameError"
+            />
+          </label>
           {errors.nameError && <ErrorMessage message={errorMessages.name} />}
-        </label>
 
-        <label>
-          Email:{' '}
-          <input
-            type="email"
-            name="email"
-            value={formValues.email}
-            onChange={handleInputChange}
-          />
-        </label>
+          <label className="flex justify-end">
+            <span className="pr-2">Email: </span>
+            <input
+              type="email"
+              name="email"
+              value={formValues.email}
+              onChange={handleInputChange}
+            />
+          </label>
 
-        <label>
-          Favorite framework:{' '}
-          <select
-            name="framework"
-            value={formValues.framework}
-            onChange={handleInputChange}
-          >
-            <option value="">Select framework</option>
-            {frameworkOptions.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </label>
+          <label className=" flex justify-end">
+            <span className="pr-2">Favorite Framework: </span>
+            <select
+              name="framework"
+              value={formValues.framework}
+              onChange={handleInputChange}
+            >
+              <option value="">Select framework</option>
+              {frameworkOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </label>
 
-        <button className="bg-slate-100 w-[100px]" type="submit">
-          Submit
-        </button>
-
-        <div>{responseSentence}</div>
-      </div>
-    </form>
+          <button className="bg-slate-100 " type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+      {responseSentence && (
+        <div className="p-3 mt-6 bg-green-100 rounded">{responseSentence}</div>
+      )}
+    </>
   )
 }
 
